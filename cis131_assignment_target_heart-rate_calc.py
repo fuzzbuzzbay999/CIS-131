16d8a3d013efa9ec76aa4cfbda6c6280759cbb5e
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-08-22 21:04:12 -0700

    cis131_assignment_target_heart-rate_calc

--- cis131_assignment_target_heart-rate_calc.py
+++ cis131_assignment_target_heart-rate_calc.py
@@ -9,4 +9,4 @@ target_heart_rate_lower = round(max_heart_rate * 0.5)
 target_heart_rate_upper = round(max_heart_rate * 0.85)
 print("Your maximum heart rate should be", max_heart_rate, "beats per minute")
 print("Your target heart rate is between", target_heart_rate_lower, "and", target_heart_rate_upper, "beats per minute")
-# CIS 131 - Homework 2
+
