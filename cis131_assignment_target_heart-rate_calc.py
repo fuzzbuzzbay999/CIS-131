12966d2c29c3a1cf989afabbe967cf5874c0e97b
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-08-22 21:10:13 -0700

    modified:   cis131_assignment_target_heart-rate_calc.py

--- cis131_assignment_target_heart-rate_calc.py
+++ cis131_assignment_target_heart-rate_calc.py
@@ -4,9 +4,9 @@
 # This program calculates the min and max target heart rate based on age
 
 age = input("Enter your age in years: ")
-max_heart_rate = 220 - int(age)
-target_heart_rate_lower = round(max_heart_rate * 0.5)
-target_heart_rate_upper = round(max_heart_rate * 0.85)
-print("Your maximum heart rate should be", max_heart_rate, "beats per minute")
-print("Your target heart rate is between", target_heart_rate_lower, "and", target_heart_rate_upper, "beats per minute")
+max_rate = 220 - int(age)
+target_lower = round(max_rate * 0.5)
+target_upper = round(max_rate * 0.85)
+print("Your maximum heart rate should be", max_rate, "beats per minute")
+print("Your target heart rate is between", target_lower, "and", target_upper, "beats per minute")
 
