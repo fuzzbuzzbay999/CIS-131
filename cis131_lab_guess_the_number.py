50c8ef78011ae8b0ed6c05c1f9969428bf0e0341
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-08-26 14:21:20 -0700

    i dont know what its doing

--- cis131_lab_guess_the_number.py
+++ cis131_lab_guess_the_number.py
@@ -4,19 +4,28 @@
 # This program is a "guess the number" game where the user tries to guess a randomly generated number between 1 and 1000 in 10 tries or less.
 
 from random import randint
+#generate random number
 num=int(randint(1,1000))
+#initialize variables
 guess=0
 Gcount=0
+#loop until user guesses the number or exceeds 10 tries
 while guess!=num:
+    #prod user for a guess
     guess=int(input("Enter your guess between 1 and 1000: "))
+    #if the guess is too low, too high, or correct, give feedback
     if guess<num:
         print("Too low, try again")
     elif guess>num:
         print("Too high, try again")
     else:
+        #wahoo!!
         print("Congratulations! You guessed the number.")
+    #increment the number of tries
     Gcount+=1
+    #if tries exceed 10, end the game
     if(Gcount>10 and guess!=num):
         print("Sorry, you should be able to do better.")
         print(f"The number was {num}")
+        #end the loop 
         break
