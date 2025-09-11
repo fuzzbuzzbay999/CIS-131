faac547ca595bc43b07914e4394701ef976120d8
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-09-10 20:52:20 -0700

    fixed these to better tend twards the requirements

--- cis131_lab_guess_the_number.py
+++ cis131_lab_guess_the_number.py
@@ -1,31 +1,38 @@
-3# cis131_lab_guess_the_number.py CIS 131 - lab 2
-# Declan Juliano
-# Date: 8/26/2025
-# This program is a "guess the number" game where the user tries to guess a randomly generated number between 1 and 1000 in 10 tries or less.
+'''
+script: cis131_lab_guess_the_number.py CIS 131 - lab 2
+action: This program is a "guess the number" game where the user tries to guess a randomly generated number between 1 and 1000 in 10 tries or less.
+Author: Declan Juliano
+Date:   8/26/2025, 
+        ammended 9/10 (doc strings added) 
+'''
+
 
 from random import randint
-#generate random number
+
+#generate the random number
 num=int(randint(1,1000))
 #initialize variables
 guess=0
 Gcount=0
+maxCount=10
 #loop until user guesses the number or exceeds 10 tries
 while guess!=num:
     #prod user for a guess
     guess=int(input("Enter your guess between 1 and 1000: "))
-    #if the guess is too low, too high, or correct, give feedback
-    if guess<num:
-        print("Too low, try again")
-    elif guess>num:
-        print("Too high, try again")
-    else:
-        #wahoo!!
-        print("Congratulations! You guessed the number.")
     #increment the number of tries
     Gcount+=1
+    #if the guess is too low, too high, or correct, give feedback
+    if guess<num:
+        print(f"Too low, try again, you have {maxCount-Gcount} tries left")
+    elif guess>num:
+        print(f"Too high, try again, you have {maxCount-Gcount} tries left")
+    else:
+        #wahoo!!
+        print(f"Congratulations! You guessed the number in {Gcount} tries")
+
     #if tries exceed 10, end the game
-    if(Gcount>10 and guess!=num):
+    if(Gcount>maxCount and guess!=num):
         print("Sorry, you should be able to do better.")
         print(f"The number was {num}")
-        #end the loop 
+        #end the loop due to the fail condition being met and the guess not equaling the number
         break
--- cis131_lab_miles_per_gallon.py
+++ cis131_lab_miles_per_gallon.py
@@ -1,10 +1,15 @@
-# CIS 131 - lab 1
-# Declan Juliano
-# Date: 8/22/2025
-# This program calculates the average miles per gallon for multiple trips
+'''
+script: cis131_lab_miles_per_gallon.py CIS 131 - lab 1
+action: This program calculates the average miles per gallon for multiple trips
+Author: Declan Juliano
+Date:   8/22/2025, 
+        ammended 9/10 (doc strings added and faulty logic fixed) 
+'''
 
 #initiate the variables
 isRunning = True
+totalMiles = 0
+totalGallons = 0
 Ampg = 0
 
 # Loop to get user input and calculate miles per gallon
@@ -13,16 +18,22 @@ while(isRunning):
     gallons = float(input("Enter number of gallons used (-1 to end): "))
     #if sentinel value is entered, exit the loop
     if gallons == -1:
+        #set isRunning to False to prevent any undeeded errors if this script gets used for anything else.
         isRunning = False
+        #exit the loop and proceed
         break
     #prompt user for miles driven
     miles = float(input("Enter number of miles driven: "))
     #calculate miles per gallon and print the result
     mpg = miles / gallons 
     print(f"The miles/gallon for this tank was {mpg:.6f}")
-    #calculate the overall average mpg sets to mpg if first iteration
-    Ampg = (Ampg + mpg) / 2  if Ampg != 0 else mpg 
+    #add the miles and gallons to the running total
+    totalMiles += miles
+    totalGallons += gallons
 
-#after exiting the loop, print the overall average mpg
-if(not isRunning):
+
+#after exiting the loop, calculate the average mpg aslong as both are greater than zero. (so no divide by zero error happens) if the loop is imediately terminated
+if(totalMiles>0 and totalGallons>0): 
+    Ampg=totalMiles/totalGallons
+    # print the average miles per gallon
     print(f"The overall average miles/gallon was: {Ampg:.6f}")
