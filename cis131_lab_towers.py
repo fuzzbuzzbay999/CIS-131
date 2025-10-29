ceeaf65da05b29cbfaed25334b498f9962aa7eeb
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-10-28 20:12:28 -0700

    cis131_lab_towers

--- cis131_lab_towers.py
+++ cis131_lab_towers.py
@@ -5,11 +5,15 @@ Date:   10/28/2025
 '''
 
 def hanoi(n, source, target, auxiliary):
+    # If final disc, move to the target
     if n == 1:
         print(f"{source} → {target}")
     else:
+        # move the next disc from the source to the auxiliary peg using the target peg as a stash
         hanoi(n - 1, source, auxiliary, target)
+        # print the movement
         print(f"{source} → {target}")
+        # move the same disc to the target using the source peg as a stash
         hanoi(n - 1, auxiliary, target, source)
 
 # Move 64 disks from peg 1 to peg 3
