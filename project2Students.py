4e2b886089c4148b2dc15e53430de14d2a30b681
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-11-11 12:54:49 -0700

    project

--- project2Students.py
+++ project2Students.py
@@ -15,7 +15,9 @@ from IPython import embed
 class Person(ABC):
     # define the class (first, last, id, email, phone)
     def __init__(self, first, last, id, email, phone):
-
+        """Initialize Person
+            args: abstract
+        """
         # If any of the checks throw an error raise it and continue
         try:
             self._first = first
@@ -45,32 +47,53 @@ class Person(ABC):
     # Get first name        
     @property
     def firstName(self):
+        """Returns the first name
+        args: none
+        """
         return self._first
     @firstName.setter
     def firstName(self,first):
+        """Sets the first name
+        args: first (str): first name
+        """
         self._first = first
 
     # Get last name
     @property
     def lastName(self):
+        """Returns the last name
+        args: none
+        """
         return self._last
     @lastName.setter
     def lastName(self,last):
+        """Sets the last name
+        args: last (str): last name
+        """
         self._last = last
 
     # Get id number
     @property
     def idNumber(self):
+        """Returns the ID number
+        args: none
+        """
         return self._id
     
     # Get email
     @property
     def emailAddress(self):
+        """Returns the email address
+        args: none
+        """
         return self._email
     
     # Get phone number (if changing and its not 12 digits dont change it)
     @property
     def phoneNumber(self):
+        """Returns the phone number
+        args: none
+        """
         return self._phoneNumber
     @phoneNumber.setter
     def phoneNumber(self, phone):
@@ -81,8 +104,12 @@ class Person(ABC):
     
     # Print
     def __repr__(self):
+        """Returns the representation of the person
+        """
         return f'{self._first} {self._last} {self._email}'
     def __str__(self):
+        """Returns the string of the person
+        """
         return f'{self._first} {self._last}'
 
 
@@ -98,7 +125,19 @@ class Employee(Person):
 
     # Define the class with people(first, last, id, email, phone) and This(month, day, year, salary, role, classification)
     def __init__(self, first: str, last: str, id: int|str, email: str,phone: str, month: int,day: int, year: int, salary: float,role: str, classification: str):
-        
+        """Initialize Employee
+            args: first (str): first name
+                  last (str): last name
+                  id (int|str): 4 digit id number
+                  email (str): email address
+                  phone (str): phone number in the format xxx-xxx-xxxx
+                  month (int): hire month
+                  day (int): hire day
+                  year (int): hire year
+                  salary (float): annual salary >=0
+                  role (str): role in roleDictionary
+                  classification (str): classification in classificationDictionary
+        """
         # Use the parent
         super().__init__(first, last, id, email,phone)
 
@@ -135,14 +174,23 @@ class Employee(Person):
     # Get hire date
     @property
     def hireDate(self):
+        """Returns the hire date
+        args: none
+        """
         return self._hireDate
     
     # Get annual Salary (if changing and the salary is negative display why and dont change it)
     @property
     def annualSalary(self):
+        """Returns the annual salary
+        args: none
+        """
         return self._annualSalary
     @annualSalary.setter
     def annualSalary(self,salary):
+        """Sets the annual salary
+        args: salary (float): annual salary >=0
+        """
         if (salary >= 0):
             self._annualSalary = round(salary,2)
         else:
@@ -151,9 +199,15 @@ class Employee(Person):
     # Get role (if changing it, and the role doesnt exist, display it and dont change it)
     @property
     def rolePerson(self):
+        """Returns the role
+        args: none
+        """
         return self.roleDictionary.get(self._role[0])
     @rolePerson.setter
     def rolePerson(self,role):
+        """Sets the role
+        args: role (str): role in roleDictionary
+        """
         isRole = [key for key, val in self.roleDictionary.items() if val == role]
         if(isRole):  
             self._role = isRole
@@ -163,9 +217,15 @@ class Employee(Person):
     # Get classification (if changing it, and the classificaiton doesnt exist, display it and dont change it)
     @property
     def classificationPerson(self):
+        """Returns the classification
+        args: none
+        """
         return self.classificationDictionary.get(self._classification[0])
     @classificationPerson.setter
     def classificationPerson(self,classification):
+        """Sets the classification
+        args: classification (str): classification in classificationDictionary
+        """
         isClassification = [key for key, val in self.classificationDictionary.items() if val == classification]
         if(isClassification):
             self._classification = isClassification
@@ -174,10 +234,32 @@ class Employee(Person):
 
     # Print
     def __repr__(self):
+        """Returns the representation of the employee
+        """
         return f'{self._first} {self._last} {self._email}'
     def __str__(self):
+        """Returns the string of the employee
+        """
         return f'{self._first} {self._last}'
 
+
+
+#**************student**************
+class Student(Person):
+    # Define the class with people(first, last, id, email, phone)
+    def __init__(self, first, last, id, email, phone):
+        """Initialize Student
+            args: first (str): first name
+                  last (str): last name
+                  id (int|str): 4 digit id number
+                  email (str): email address
+                  phone (str): phone number in the format xxx-xxx-xxxx
+        """        # Use the parent
+
+        super().__init__(first, last, id, email, phone)
+    
+    # Inherit all methods
+    
 
 
 '''
@@ -195,6 +277,10 @@ isRunning = True    # Global state flag
 
 # Function to itterate through supplied text document and populate the list (File name to read)
 def getEmployees(file): 
+    """Populate the employeeList from a text document
+        args: file (str): text document to read
+    """
+
     global employeeList     # Ensure global control of employeeList
     employeeList = []       # Reset employeeList
     employees = open(file)  # Open the text document
@@ -227,7 +313,38 @@ def getEmployees(file):
                 print(f'Added employee {first} {last}')     # Display current progress
             except:
                 print ('skipping employee')
+            
+# Function to itterate through supplied text document and populate the list (File name to read)
+def getStudents(file):
+    """Populate the studentList from a text document
+        args: file (str): text document to read
+    """
+    global studentList      # Ensure global control of studentList
+    studentList = []        # Reset student list
+    students = open(file)   # Open the text document
+    
+    #  Read all lines and itterate through them
+    for i in students.readlines():
+            
+            # Remove all (\t, ,/) symbols and their duplicates, and replace them with commas. Strip any new lines. Split allong the commas into a list
+            student = re.sub(r'[\t /]+',',',i).strip().split(',')
 
+            # If the sliced line is not 5 or have atleast 1 digit discard it (doesnt have the proper data feilds. Either its missing them or its a heading)
+            if(len(student) == 5 and re.search(r'\d', i)):
+                # Assign the list indecies to their variables (convert those that need to be int and float to such)
+                last = student[0]
+                first = student[1]
+                id = int(student[2])
+                email = student[3]
+                phone = student[4]
+
+                # Atempt to make a new class object and append it to the employeeList. (if any errors are thrown durring initialization, then print them and skip the employee)
+                try:
+                    std =  Student(last,first,id,email,phone)
+                    studentList.append(std)
+                    print(f'Added student {first} {last}')      # Display current progress
+                except:
+                    print('skipping student')
                
 '''
 Output
@@ -238,14 +355,20 @@ from datetime import date
 # Function for the menu, (num[amount of choices], items[list of what the items are])
 # It is set up this way to allow for more modularity when presented with sub menus and what not
 def createMenu():
+    """Create the menu for user interaction
+    """
     
     # Menu items
-    items = ['Quit','Display Employee Employment Information','Display Employee Contact Information']
+    items = ['Quit','Display Employee Employment Information','Display Employee Contact Information','Display Student Contact Information','Display All Person Contact Information']
     # Number of menu items
     num = len(items)
     
     # Display the numeric options using the above amounts
     def options(items):
+        """Display the options to the user
+            args: items (list): list of menu items
+        """
+        
         print('\n')
         print("Please select an option below\n")
         for i in range(0,num):
@@ -253,6 +376,9 @@ def createMenu():
 
     # Switch case for optioins
     def choices(index):
+        """Select the action based on user input
+            args: index (str): user input
+        """
         match index:
             # Compare the number with the assosiated action
             case '1':    # Option 1
@@ -261,7 +387,6 @@ def createMenu():
                 global isRunning    # Ensure global control
                 isRunning = False   # Trip the flag
                 
-
             case '2':  # Option 2
                 # Header
                 print(f'{"LastName":<20}{"FirstName":<20}{"ID":<20}{"Email":<30}{"Phone":<20}{"HireDate":<20}{"Classification":<20}{"Role":<20}{"Salary":<20}')
@@ -275,22 +400,41 @@ def createMenu():
                 # Body
                 for emp in employeeList:
                     print(f'{emp.lastName:<20}{emp.firstName:<20}{emp.idNumber:<20}{emp.phoneNumber:<20}')
-            
+
+            case '4': # Option 4
+                # Header
+                print(f'{"LastName":<20}{"FirstName":<20}{"ID":<20}{"Phone":<20}')
+                # Body
+                for std in studentList:
+                    print(f'{std.lastName:<20}{std.firstName:<20}{std.idNumber:<20}{std.phoneNumber:<20}')
+
+            case '5': # Option 5
+                # Header              
+                print(f'{"LastName":<20}{"FirstName":<20}{"ID":<20}{"Phone":<20}')
+                # Body
+                people = employeeList+studentList
+                for p in people:
+                    print(f'{p.lastName:<20}{p.firstName:<20}{p.idNumber:<20}{p.phoneNumber:<20}')
             case _: # No choice for that input
                 print(f"I am sorry, {index} is not an option")
     
     # display the options and prompt for input
     def getChoice():
+        """Get the user choice
+            args: none
+        """
         options(items)
         return input()
 
     # While running call getChoice() and select the input
     while(isRunning):
         choice = getChoice()
+        print('') # New line before the output
         choices(choice)
 
-# Populate the employeeList
+# Populate employeeList and studentList
 getEmployees("employees.txt")
+getStudents("students.txt")
 
 # Promp the user with the options of (quit, employment info, contact info)
 createMenu()
